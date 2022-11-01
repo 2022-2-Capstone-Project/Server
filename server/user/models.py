7a74e2f057cd5a0eee11d372867c66115efc997a
SonMinGyu <littleking13@naver.com>
2022-11-01 22:41:52 +0900

    [REFATOR] merge 후 user model 연관 관계 리팩토링

--- server/user/models.py
+++ server/user/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # 기본 유저 모델
     nickname = models.CharField(max_length=30)  # 닉네임
     bookmarks = models.ManyToManyField('tour_theme.TourTheme', blank=True)  # 북마크한 테마 (선배용)
-    likes = models.ManyToManyField('tour_theme.Tour', blank=True)  # 좋아요한 투어 (후배용)
+    likes = models.ManyToManyField('tour.Tour', blank=True)  # 좋아요한 투어 (후배용)
     follows = models.ManyToManyField('self', symmetrical=False, related_name='follower', blank=True)  # 내가 팔로우하는 유저
     followers = models.ManyToManyField('self', symmetrical=False, related_name='follow', blank=True)  # 나를 팔로우하는 유저
     user_type = models.CharField(
