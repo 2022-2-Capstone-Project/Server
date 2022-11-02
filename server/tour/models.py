9736884f84c82b2cc4b7d50babc8d1e699afc61e
SonMinGyu <littleking13@naver.com>
2022-11-02 11:18:21 +0900

    [REFACTOR] tour model 리팩토링

--- server/tour/models.py
+++ server/tour/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from user.models import Profile
+from tour_theme.models import TourTheme
 
 # Create your models here.
 class Tour(models.Model):
@@ -8,14 +9,20 @@ class Tour(models.Model):
     created = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
     thumbnail = models.URLField(verbose_name="썸네일")
     description = models.TextField(verbose_name="투어설명")
-    # user_id = models.ForeignKey(
-    #     "user.User",
-    #     # related_name="user",
-    #     on_delete=models.CASCADE(),
-    #     db_column="user_id",
-    #     verbose_name="생성유저"
-    # )
-    # themeId = models.OneToOneField("Theme", on_delete=models.CASCADE(), verbose_name="테마이름")
+    profile_id = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        db_column="profile_id",
+        verbose_name="생성유저",
+        default=''
+    )
+    themeId = models.ForeignKey(
+        TourTheme,
+        on_delete=models.CASCADE,
+        db_column="theme_id",
+        verbose_name="테마이름",
+        default=''
+    )
 
     class Meta:
         db_table = "tour"
