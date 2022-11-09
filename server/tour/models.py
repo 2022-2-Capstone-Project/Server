8926ae26b837aad9109584841edb9ba098cd9c7a
SonMinGyu <littleking13@naver.com>
2022-11-09 16:44:51 +0900

    [REFACTOR] tour model 리팩토링
    - tour model을 리팩토링함

--- server/tour/models.py
+++ server/tour/models.py
@@ -9,17 +9,17 @@ class Tour(models.Model):
     created = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
     thumbnail = models.URLField(verbose_name="썸네일")
     description = models.TextField(verbose_name="투어설명")
-    participants = models.PositiveIntegerField()  # 권장 참여자 수
-    start_place = models.CharField(max_length=100)  # 시작 장소
-    latitude = models.FloatField()  # 위도
-    longitude = models.FloatField()  # 경도
+    participants = models.PositiveIntegerField(verbose_name="정원")  # 설정 참여자 수
+    start_place = models.CharField(max_length=100, verbose_name="시작장소")  # 시작 장소
+    latitude = models.FloatField(verbose_name="시작장소 위도")  # 위도
+    longitude = models.FloatField(verbose_name="시작장소 경도")  # 경도
     profile_id = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
         db_column="profile_id",
         verbose_name="생성유저"
     )
-    themeId = models.ForeignKey(
+    theme_id = models.ForeignKey(
         TourTheme,
         on_delete=models.CASCADE,
         db_column="theme_id",
