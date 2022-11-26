0df4c896aef583f1bb27fab527ffa79333fedba1
SonMinGyu <littleking13@naver.com>
2022-11-26 11:48:18 +0900

    [REFACTOR] tour_theme model field 리팩토링
    - tour_theme model created field를 자동으로 추가되도록 리팩토링

--- server/tour_theme/models.py
+++ server/tour_theme/models.py
@@ -6,7 +6,7 @@ from user.models import Profile
 class TourTheme(models.Model):
     title = models.CharField(max_length=100)  # 테마명
     author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)  # 작성자
-    created = models.DateTimeField(auto_created=True, blank=True, null=True)  # 생성일
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)  # 생성일
     estimated = models.IntegerField()  # 예상 소요 시간 (minutes)
     participants = models.PositiveIntegerField()  # 권장 참여자 수
     start_place = models.CharField(max_length=100)  # 시작 장소
