21c796f5a232af359c3a6045d9c3ccea5df3f6a6
jongwonyang <yjw963@gmail.com>
2022-12-02 20:09:56 +0900

    local connection fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-a8iqvx*hcce&wvv8x7!rw_w+swvkhxtf+&_g=bmqpejz)i1g1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2']
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1']
 
 
 # Application definition
