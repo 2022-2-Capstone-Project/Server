65bd09ae480f23c22172f5f5cfd900923bc52444
SonMinGyu <littleking13@naver.com>
2022-12-07 00:55:08 +0900

    [REFACTOR] tour model field 리팩토링
    - tour model의 thumnail field를 imageField 형식으로 리팩토링함

--- server/tour/models.py
+++ server/tour/models.py
@@ -8,7 +8,7 @@ class Tour(models.Model):
     tourName = models.CharField(max_length=256, verbose_name="투어이름")
     date = models.CharField(max_length=50, verbose_name="투어일시")
     created = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
-    thumbnail = models.URLField(verbose_name="썸네일")
+    thumbnail = models.ImageField(upload_to='images/', blank=True, null=True)  # 썸네일
     description = models.TextField(verbose_name="투어설명")
     participants = models.PositiveIntegerField(verbose_name="정원")  # 설정 참여자 수
     start_place = models.CharField(max_length=100, verbose_name="시작장소")  # 시작 장소
